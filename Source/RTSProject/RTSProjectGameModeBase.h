dbb49ff22d56a4b349198df5909c0c04475e925b
Graysire <Graysire@dyrix.net>
2022-03-16 22:07:08 -0700

    Initial Project commit

---
+++ Source/RTSProject/RTSProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RTSProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RTSPROJECT_API ARTSProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
